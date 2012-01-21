82b80be30d3988dbaa09122003a495d361ecec85
Andrew Taylor <andy@brekka.co.uk>
2012-01-21 22:56:23 +0000

    git-svn-id: svn+ssh://titan.n3rv.co.uk/home/brekka/svn/phalanx/trunk@371 d624a0b8-0206-0410-91b0-81c552a60902

--- core/src/test/java/org/brekka/phalanx/services/impl/AsymmetricCryptoServiceImplTest.java
+++ core/src/test/java/org/brekka/phalanx/services/impl/AsymmetricCryptoServiceImplTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.UUID;
 
 import net.iharder.Base64;
 
@@ -12,14 +11,12 @@ import org.brekka.phalanx.dao.AsymmetricKeyPairDAO;
 import org.brekka.phalanx.dao.CryptoDataDAO;
 import org.brekka.phalanx.model.AsymedCryptoData;
 import org.brekka.phalanx.model.AsymmetricKeyPair;
-import org.brekka.phalanx.model.IdentifiableEntity;
 import org.brekka.phalanx.model.Principal;
 import org.brekka.phalanx.model.PrivateKeyToken;
 import org.brekka.phalanx.profile.CryptoProfileRegistry;
 import org.brekka.phalanx.profile.impl.CryptoProfileRegistryImpl;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class AsymmetricCryptoServiceImplTest {
 
@@ -33,7 +30,7 @@ public class AsymmetricCryptoServiceImplTest {
     public void setUp() throws Exception {
         CryptoProfileRegistry cryptoProfileRegistry = CryptoProfileRegistryImpl.createBasicRegistry();
         CryptoDataDAO cryptoDAO = new TestCryptoDataDAO();
-        AsymmetricKeyPairDAO asymmetricKeyPairDAO = Mockito.mock(AsymmetricKeyPairDAO.class);
+        AsymmetricKeyPairDAO asymmetricKeyPairDAO = new TestAsymmetricKeyPairDAO();
         
         symService = new SymmetricCryptoServiceImpl();
         symService.setCryptoDataDAO(cryptoDAO);
@@ -58,7 +55,6 @@ public class AsymmetricCryptoServiceImplTest {
         String password = "password";
         Principal user = new Principal();
         AsymmetricKeyPair asyncKeyPair = service.generateKeyPair(password, user);
-        assignIds(asyncKeyPair, asyncKeyPair.getPrivateKey(), asyncKeyPair.getPublicKey());
         
         // Extract the private Key from what was created
         PrivateKeyToken privateKeyToken = service.decrypt(asyncKeyPair, password);
@@ -69,10 +65,5 @@ public class AsymmetricCryptoServiceImplTest {
         assertTrue(Arrays.equals(data, result));
     }
     
-    private void assignIds(IdentifiableEntity... entity) {
-        for (IdentifiableEntity identifiableEntity : entity) {
-            identifiableEntity.setId(UUID.randomUUID());
-        }
-    }
 
 }
---
+++ core/src/test/java/org/brekka/phalanx/services/impl/TestAsymmetricKeyPairDAO.java
@@ -0,0 +1,37 @@
+package org.brekka.phalanx.services.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.brekka.phalanx.dao.AsymmetricKeyPairDAO;
+import org.brekka.phalanx.model.AsymmetricKeyPair;
+
+public class TestAsymmetricKeyPairDAO implements AsymmetricKeyPairDAO {
+
+    private Map<UUID, AsymmetricKeyPair> map = new HashMap<UUID, AsymmetricKeyPair>();
+    
+    @Override
+    public AsymmetricKeyPair retrieveById(UUID entityId) {
+        return map.get(entityId);
+    }
+
+    @Override
+    public UUID create(AsymmetricKeyPair entity) {
+        UUID uuid = UUID.randomUUID();
+        entity.setId(uuid);
+        map.put(uuid, entity);
+        return uuid;
+    }
+
+    @Override
+    public void update(AsymmetricKeyPair entity) {
+        
+    }
+
+    @Override
+    public void delete(UUID entityId) {
+        map.remove(entityId);
+    }
+
+}
